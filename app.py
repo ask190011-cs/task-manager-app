77fcf2873c82038e062573392c834b401bd3000f
A K <anupamkulkarni1@gmail.com>
2026-03-24 12:16:11 -0500

    Remove unnecessary dependencies

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 from flask import session
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
